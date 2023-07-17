9bc8911c20a3e4d40828713b67ff909b949fcdcd
Ahthesham Ali <ahtheshamali786@gmail.com>
2023-07-17 06:16:07 -0400

    finished Website

---
+++ sanity/sanity.cli.ts
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: '96q327pi',
+    dataset: 'production'
+  }
+})
--- sanity/tailwind.config.js
+++ sanity/tailwind.config.js
@@ -9,6 +9,6 @@ module.exports = {
   theme: {},
   plugins: [require("@tailwindcss/typography")],
 
-  
+ 
 
 };
